5b930ae929aae98ef12cc6fd37f3c7f4b770655f
Shashank Bharadwaj <shashu10@gmail.com>
2017-03-04 03:05:35 -0800

    getting all attributes

--- api/resolver.js
+++ api/resolver.js
@@ -12,13 +12,28 @@ function getTitle(body) {
     // const body = "<title>Gold 80000mAh Dual USB Portable Solar Battery Charger Power Bank For Cell Phone  | eBay</title>";
     return regexSearch(body, regex);
 }
-function getRating(body) {
-    const regex = /ebay-review-start-rating\">[\s]*([\d\.]+)[\s]*<\/span>/;
-    body = "<span class=\"ebay-review-start-rating\">\n4.6</span>\n<div class=\"ebay-star-rating\">";
-    console.log("starting search");
+function getRatings(body) {
+    var regex = /ebay-review-start-rating\">[\s]*([\d\.]+)[\s]*<\/span>/;
+    // body = "<span class=\"ebay-review-start-rating\">\n4.6</span>\n<div class=\"ebay-star-rating\">";
     return regexSearch(body, regex);
 }
-
+function getImageURL(body) {
+    var regex = /og:image"[\s]*[cC]ontent="([^"]+)"/;
+    // body = "<meta property=\"og:image\" content=\"http://i.ebayimg.com/images/i/131644763083-0-1/s-l1000.jpg\">";
+    return regexSearch(body, regex);
+}
+function getReviews(body) {
+    var regex = /ebay-reviews-count">[^\d]*(\d*[^<]+)<\/span>/;
+    // body = "<span class=\"ebay-reviews-count\">average based on 15 product ratings</span>";
+    return regexSearch(body, regex);
+}
+function getPrice(body) {
+    var regex = /id=\"prcIsum"[^>]*>([^<]+)<\/span>/;
+    // body = "<span class=\"notranslate\" id=\"prcIsum\" itemprop=\"price\" style=\"\" content=\"9.89\">US $9.89</span>";
+    return regexSearch(body, regex);
+}
+// <meta property="og:image" content="http://i.ebayimg.com/images/i/131644763083-0-1/s-l1000.jpg">
+// console.log(getRatings(""));
 // class fetcher extends AnotherClass {
 //     constructor(argument) {
 //         // code...
@@ -55,15 +70,19 @@ module.exports = function (req, res) {
     request(url, function (error, response, body) {
         if (!error) {
             console.log("used");
-            // console.log(body);
+            console.log(body);
             // readHTML(body);
             console.log(getTitle(body));
-            console.log(getRating(body));
+            console.log(getRatings(body));
+            console.log(getReviews(body));
+            console.log(getImageURL(body));
+            console.log(getPrice(body));
             // const image = response.body.data.images.original;
             // const width = image.width > 600 ? 600 : image.width;
-            var html = "<div><img src=\"https://d1qb2nb5cznatu.cloudfront.net/users/572921-large?1440654205\"><h1>Shashank Bharadwaj</h1><p>3 years developing complex full-stack javascript applications, managing small teams, and building products from the ground up.</p></div>";
+            var html = "<div style=\"display: -webkit-box\"><div><img src=\"http://i.ebayimg.com/images/i/131644763083-0-1/s-l1000.jpg\" width=\"100px\"></div><div><h4>Gold 80000mAh Dual USB Portable Solar Battery Charger Power Bank For Cell Phone</h4><p>US $9.89</p><a href=\"http://google.com\" target=\"_blank\">4.89</a><a href=\"http://google.com\" target=\"_blank\">15 product ratings</a></div></div>";
             res.json({
                 body: html
+                // Add raw:true if you're returning content that you want the user to be able to edit
             });
         }
         else {
